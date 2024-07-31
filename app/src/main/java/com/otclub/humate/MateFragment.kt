a26b4cf432093d33ed67c6bed1239e23f37352fe
wlgus253254 <wlgus253254@ewhain.net>
2024-08-01 02:02:14 +0900

    :sparkles: Feat: 툴바 생성

--- app/src/main/java/com/otclub/humate/MateFragment.kt
+++ app/src/main/java/com/otclub/humate/MateFragment.kt
@@ -4,8 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatImageButton
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import com.otclub.humate.databinding.FragmentMateBinding
+
 
 class MateFragment : Fragment() {
 
@@ -21,6 +27,26 @@ class MateFragment : Fragment() {
         mBinding = binding
 
         return mBinding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val activity = activity as? MainActivity
+        activity?.let {
+            val toolbar = it.getToolbar() // MainActivity의 Toolbar를 가져옴
+            val leftButton: ImageButton = toolbar.findViewById(R.id.left_button)
+            val rightButton: Button = toolbar.findViewById(R.id.right_button)
+
+            // 버튼의 가시성 설정
+            val showLeftButton = false
+            val showRightButton = true
+            leftButton.visibility = if (showLeftButton) View.VISIBLE else View.GONE
+            rightButton.visibility = if (showRightButton) View.VISIBLE else View.GONE
+
+            // 액션 바의 타이틀을 설정하거나 액션 바의 다른 속성을 조정
+            it.setToolbarTitle("메이트")
+        }
     }
 
     override fun onDestroyView() {
