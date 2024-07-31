fd5482c8196a6a6d9725f76c109e9f58d7d7f381
Hypersand <on8214@gmail.com>
2024-07-31 18:13:02 +0900

    ✨ Feat: 내비게이션 바 구현

---
+++ app/src/main/java/com/otclub/humate/MyPageFragment.kt
@@ -0,0 +1,31 @@
+package com.otclub.humate
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.otclub.humate.databinding.FragmentMyPageBinding
+
+
+class MyPageFragment : Fragment() {
+
+    private var mBinding : FragmentMyPageBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?): View? {
+
+        val binding = FragmentMyPageBinding.inflate(inflater, container, false)
+
+        mBinding = binding
+
+        return mBinding?.root
+    }
+
+    override fun onDestroyView() {
+        mBinding = null
+        super.onDestroyView()
+    }
+}
