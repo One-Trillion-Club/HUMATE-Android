afbb298896604db4016fa2e797ff65f44024c0ac
Hypersand <on8214@gmail.com>
2024-08-04 17:01:56 +0900

    ✨ Feat: request 시 쿠키 담아서 보내도록 설정

--- app/src/main/java/com/otclub/humate/retrofit/RetrofitConnection.kt
+++ app/src/main/java/com/otclub/humate/retrofit/RetrofitConnection.kt
@@ -1,7 +1,10 @@
 package com.otclub.humate.retrofit
 
+import okhttp3.JavaNetCookieJar
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.net.CookieManager
 
 class RetrofitConnection {
 
@@ -10,9 +13,14 @@ class RetrofitConnection {
         private const val BASE_URL = "http://10.0.2.2:8080/"
         private var INSTANCE: Retrofit? = null
 
+        val client = OkHttpClient.Builder()
+            .cookieJar(JavaNetCookieJar(CookieManager()))
+            .build()
+
         fun getInstance(): Retrofit {
             if (INSTANCE == null) {
                 INSTANCE = Retrofit.Builder()
+                    .client(client)
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build()
