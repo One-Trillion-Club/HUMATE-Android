af6e5d43f0c7d03c5c6d8985651f976c9ca8b640
yookyung Choi <ykc131719@gmail.com>
2024-08-05 23:35:08 +0900

    ✨ Feat: toolbar 뒤로가기 버튼 활성화, 3개 점버튼 클릭 동작

--- app/src/main/java/com/otclub/humate/chat/fragment/ChatRoomFragment.kt
+++ app/src/main/java/com/otclub/humate/chat/fragment/ChatRoomFragment.kt
@@ -1,18 +1,15 @@
 package com.otclub.humate.chat.fragment
 
-import android.graphics.Color
-import android.graphics.Typeface
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
+import com.google.android.material.tabs.TabLayout
 import com.otclub.humate.MainActivity
 import com.otclub.humate.R
 import com.otclub.humate.chat.adapter.ChatRoomAdapter
@@ -22,7 +19,6 @@ import com.otclub.humate.databinding.ChatRoomFragmentBinding
 class ChatRoomFragment  : Fragment()  {
     private val chatRoomViewModel: ChatRoomViewModel by activityViewModels()
     private var mBinding: ChatRoomFragmentBinding? = null
-    private var selectedButton: Button? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,6 +32,36 @@ class ChatRoomFragment  : Fragment()  {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        mBinding?.tabLayout?.addTab(mBinding!!.tabLayout.newTab().setText(getString(R.string.chat_mate_list)))
+        mBinding?.tabLayout?.addTab(mBinding!!.tabLayout.newTab().setText(getString(R.string.chat_pending_list)))
+        mBinding?.tabLayout?.getTabAt(1)?.select()
+
+        mBinding?.tabLayout?.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                when (tab?.position) {
+                    0 -> {
+                        // "채팅방 리스트" 탭 선택 시
+                        chatRoomViewModel.fetchChatRoomList("K_1")
+                        //chatRoomViewModel.setSelectedButton(R.id.mateListButton)
+                    }
+                    1 -> {
+                        // "대기 중인 채팅방 리스트" 탭 선택 시
+                        chatRoomViewModel.fetchPendingChatRoomList("K_1")
+                        //chatRoomViewModel.setSelectedButton(R.id.pendingListButton)
+                    }
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                // 탭이 선택되지 않은 경우 처리 (필요에 따라 추가)
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                // 이미 선택된 탭이 다시 선택된 경우 처리 (필요에 따라 추가)
+            }
+        })
+
+
 
         // RecyclerView 설정
         mBinding?.chatRoomRecyclerView?.apply {
@@ -69,46 +95,6 @@ class ChatRoomFragment  : Fragment()  {
                 mBinding?.chatRoomRecyclerView?.adapter = adapter
             }
         }
-
-        // 버튼 클릭 리스너 설정
-        mBinding?.mateListButton?.setOnClickListener {
-            selectButton(mBinding?.mateListButton)
-            chatRoomViewModel.fetchChatRoomList("K_1")
-            chatRoomViewModel.setSelectedButton(R.id.mateListButton)
-        }
-
-        mBinding?.pendingListButton?.setOnClickListener {
-            selectButton(mBinding?.pendingListButton)
-            chatRoomViewModel.fetchPendingChatRoomList("K_1")
-            chatRoomViewModel.setSelectedButton(R.id.mateListButton)
-        }
-
-        // 초기 상태 설정
-        if (chatRoomViewModel.selectedButton.value == null) {
-            selectButton(mBinding?.mateListButton)
-            chatRoomViewModel.fetchChatRoomList("K_1")
-            chatRoomViewModel.setSelectedButton(R.id.mateListButton)
-        }
-    }
-
-    private fun selectButton(button: Button?) {
-        // 기존 선택된 버튼이 있다면 원래 상태로 되돌리기
-        selectedButton?.let {
-            it.isSelected = false // 선택 상태를 해제
-            it.setTypeface(Typeface.DEFAULT, Typeface.NORMAL)
-            it.setTextColor(Color.BLACK)
-            it.setBackgroundResource(R.drawable.tab_basic_indicator) // 버튼 배경을 설정
-        }
-
-        // 새로 선택된 버튼의 스타일 적용
-        button?.let {
-            it.isSelected = true
-            it.setTypeface(Typeface.DEFAULT_BOLD) // 텍스트를 굵게
-            it.setTextColor(Color.BLACK) // 텍스트 색깔을 검은색으로 설정
-            it.setBackgroundResource(R.drawable.tab_indicator) // 버튼 배경을 설정
-        }
-
-        selectedButton = button
     }
 
     override fun onDestroyView() {
